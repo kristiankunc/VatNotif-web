e022af675c2163c556f3b3dd629dc952bb8fb118
Kristián Kunc <kristian.kunc@gmail.com>
2023-04-01 18:12:06 +0200

    :wheelchair: add api status to navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,8 +1,30 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	let status = "API Status - Loading...";
+
+	onMount(async () => {
+		try {
+			const res = await fetch("https://vatnotif-api.kristn.co.uk");
+			if (res.ok) {
+				status = "API Status - ✅";
+			} else {
+				status = `API Status - ❌ (${res.status})`;
+			}
+		} catch (e) {
+			status = "API Status - ❌ (Offline)";
+		}
+	});
+</script>
+
 <div class="navbar">
 	<div class="navbar__title">
 		<a href="/">VatNotif</a>
 	</div>
 
+	<div class="navrar__api_status">
+		<p contenteditable bind:innerHTML={status} />
+	</div>
 	<div class="navbar__links">
 		<a href="/auth/login">Login</a>
 		<a href="/auth/logout">Logout</a>
@@ -19,12 +41,16 @@
 		background-color: #1e1e1e;
 	}
 
-	.navbar__title a {
+	.navbar a,
+	.navbar p {
+		margin: 1rem;
 		color: #fff;
+	}
+
+	.navbar__title a {
 		font-weight: bold;
 		text-decoration: none;
 		font-size: 3rem;
-		margin: 1rem;
 	}
 
 	.navbar__links {
@@ -35,8 +61,6 @@
 	}
 
 	.navbar__links a {
-		color: #fff;
-		margin: 1rem;
 		font-size: 1.25rem;
 		text-decoration: none;
 		font-weight: bold;
