479c251149c239d2464be69bbeb1ba916cd8244d
Kristián Kunc <kristian.kunc@gmail.com>
2023-04-01 12:20:30 +0200

    :ambulance: remove config files

--- src/lib/server/conf/mysql.ts
+++
@@ -1,9 +0,0 @@
-import type { MysqlConfigOptions } from "$lib/types/config";
-
-export const mysqlConfig: MysqlConfigOptions = {
-	host: "192.168.0.104",
-	port: 3306,
-	username: "vatnotif",
-	password: "cG947$*XPrYo",
-	database: "vatnotif",
-};
