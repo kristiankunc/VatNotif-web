a347b228ff3f349cc48192c27b310dd2c7364032
Kristián Kunc <kristian.kunc@gmail.com>
2023-04-05 17:14:07 +0200

    :lipstick: make index look 100x nicer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Navbar from "$lib/components/Navbar.svelte";
 	import "../lib/styles/index.css";
+	import Layout from "./+layout.svelte";
 </script>
 
 <Navbar />
@@ -8,11 +9,11 @@
 	<title>VatNotif - Home</title>
 </svelte:head>
 <div class="main-div">
-	<div class="introduction-heading">
+	<div class="section-heading introduction-heading">
 		<h2>How to use?</h2>
 	</div>
 
-	<div class="introduction-div">
+	<div class="section introduction-div">
 		<div class="card">
 			<img src="/cards/explore.svg" alt="Avatar" style="width:100%" />
 			<div class="card-contents-div">
@@ -35,42 +36,88 @@
 			</div>
 		</div>
 	</div>
+
+	<div class="section-heading introduction-heading">
+		<h2>Getting Started</h2>
+	</div>
+
+	<div class="section">
+		<p>
+			To get started, please login with your VATSIM account.
+			<br />
+			After that you can proceed to the <a href="/dashboard">Dashboard</a> and add some callsigns you'd like to receive notifications for.
+			<br />
+			You can also add Discord webhooks to receive notifications on your discord servers.
+		</p>
+	</div>
+
+	<div class="section-heading introduction-heading">
+		<h2>FAQ</h2>
+	</div>
+
+	<div class="section faq">
+		<h3>What is VatNotif?</h3>
+		<p>VatNotif is a VATSIM notification service that allows you to receive notifications when your favourite controllers come online.</p>
+
+		<h3>How does it work?</h3>
+		<p>
+			VatNotif uses the VATSIM API to fetch the latest data on controllers and their positions. <br />It then compares this data with your
+			preferences and sends you a notification if a match is found.
+		</p>
+
+		<h3>Is the service paid?</h3>
+		<p>
+			No, VatNotif is completely free to use.<br />If you'd like to support the project, you can donate to the developer
+			<a href="https://paypal.me/kristn1">here</a>.
+		</p>
+
+		<h3>Is this project open-source?</h3>
+		<p>
+			Yes, the project is open-source and can be found on GitHub:
+			<br />
+			Web -
+			<a href="https://github.com/kristiankunc/VatNotif-web">VatNotif-web</a>
+			<br />
+			API -
+			<a href="https://github.com/kristiankunc/VatNotif-api">VatNotif-api</a>
+		</p>
+	</div>
+
+	<div class="section-heading">
+		<h2>Data Handling Policy</h2>
+	</div>
+
+	<div class="section data-handling">
+		<p>
+			At VatNotif, we take data privacy seriously and are committed to protecting your personal information. As such, we have implemented the
+			following data handling policy:
+		</p>
+		<br />
+		<ol>
+			<li>
+				<strong>Unique Identifier:</strong> We only use a unique identifier (CID) from VATSIM to identify you. We do not store any other personal
+				information.
+			</li>
+			<li>
+				<strong>No Tracking/3rd Party Cookies:</strong> We do not use any tracking or 3rd party cookies. The only cookies we use are session cookies
+				which are necessary for the website to function properly.
+			</li>
+			<li>
+				<strong>Watched Callsigns and Notification Preferences:</strong> In our database, we only store your watched callsigns and notification
+				preferences such as your Discord webhook URL. This information is used to provide you with real-time updates on your selected callsigns.
+			</li>
+			<li>
+				<strong>Data Deletion:</strong> If you wish to have your data deleted, please send an email to
+				<a href="mailto:kristian@kristn.co.uk">kristian@kristn.co.uk</a> We will promptly delete all of your stored data upon receipt of your request.
+			</li>
+		</ol>
+		<br />
+		<p>
+			<i>
+				We hope that this policy provides you with peace of mind knowing that we are committed to protecting your personal information.
+				<br />
+				If you have any questions or concerns, please do not hesitate to contact us.
+			</i>
+		</p>
+	</div>
 </div>
-
-<h1>Hello and welcome to VatNotif!</h1>
-<p>
-	To get started, please login with your VATSIM account.
-	<br />
-	After that you can proceed to the dashboard and add some callsigns you'd like to receive notifications for.
-	<br />
-	You can also add Discord webhooks to receive notifications there.
-</p>
-
-<h1>Data Handling Policy</h1>
-<p>
-	At VatNotif, we take data privacy seriously and are committed to protecting your personal information. As such, we have implemented the following
-	data handling policy:
-</p>
-<ol>
-	<li>
-		<strong>Unique Identifier:</strong> We only use a unique identifier (CID) from VATSIM to identify you. We do not store any other personal information.
-	</li>
-	<li>
-		<strong>No Tracking/3rd Party Cookies:</strong> We do not use any tracking or 3rd party cookies. The only cookies we use are session cookies which
-		are necessary for the website to function properly.
-	</li>
-	<li>
-		<strong>Watched Callsigns and Notification Preferences:</strong> In our database, we only store your watched callsigns and notification preferences
-		such as your Discord webhook URL. This information is used to provide you with real-time updates on your selected callsigns.
-	</li>
-	<li>
-		<strong>Data Deletion:</strong> If you wish to have your data deleted, please send an email to
-		<a href="mailto:kristian@kristn.co.uk">kristian@kristn.co.uk</a>. We will promptly delete all of your stored data upon receipt of your
-		request.
-	</li>
-</ol>
-<p>
-	We hope that this policy provides you with peace of mind knowing that we are committed to protecting your personal information. If you have any
-	questions or concerns, please do not hesitate to contact us.
-</p>
-<p>VatNotif</p>
