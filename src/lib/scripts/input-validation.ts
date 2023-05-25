b63136d4b629bb492e7aadb2a6d2ed37806d19dc
Kristián Kunc <kristian.kunc@gmail.com>
2023-05-25 20:14:25 +0200

    fix callsign regex

--- src/lib/scripts/input-validation.ts
+++ src/lib/scripts/input-validation.ts
@@ -1,6 +1,6 @@
 export class InputValidation {
 	public static isCallsign(callsign: string): boolean {
-		return /^[A-Z]{3,4}_?[A-Z]{1,4}_?(GND|TWR|APP|DEP|CTR|FSS|DEL|INFO|RAI|ACC)$/.test(callsign);
+		return /^[A-Z]{2,4}_?[A-Z]{1,4}_?(GND|TWR|APP|DEP|CTR|FSS|DEL|INFO|RAI|ACC)$/.test(callsign);
 	}
 
 	public static isDiscordWebhookUrl(webhookUrl: string): boolean {
