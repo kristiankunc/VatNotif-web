7fb109985a007e028d0448d11a1ce627ca22db39
Kristián Kunc <kristian.kunc@gmail.com>
2023-06-13 20:15:12 +0200

    enhance login

--- src/routes/auth/login/+page.server.ts
+++ src/routes/auth/login/+page.server.ts
@@ -4,13 +4,15 @@ import { error, redirect } from "@sveltejs/kit";
 import { vatsimConfig } from "$lib/server/conf/vatsim";
 
 export const load = (async ({ cookies }) => {
-	const authUrl = `${vatsimConfig.authUrl}/oauth/authorize?client_id=${vatsimConfig.clientId}&redirect_uri=${vatsimConfig.redirectUri}&response_type=code&scope=${vatsimConfig.scopes}`;
+	const authUrl = new URL(`${vatsimConfig.authUrl}/oauth/authorize`);
+	authUrl.searchParams.append("client_id", vatsimConfig.clientId);
+	authUrl.searchParams.append("redirect_uri", vatsimConfig.redirectUri);
+	authUrl.searchParams.append("response_type", "code");
+	authUrl.searchParams.append("scope", vatsimConfig.scopes);
 
-	const sessionId = cookies.get("session");
-	if (!sessionId) throw redirect(302, authUrl);
-
-	const session = await SessionsDatabase.getSession(sessionId);
-	if (!session) throw redirect(302, authUrl);
+	if (!cookies.get("session") || !(await SessionsDatabase.getSession(cookies.get("session")!))) {
+		throw redirect(302, authUrl.toString());
+	}
 
 	throw redirect(302, "/dashboard");
 }) satisfies PageServerLoad;
