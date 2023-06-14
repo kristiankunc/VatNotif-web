0abe3a5643672fd1f606f1185e05e96a44b64f74
Kristián Kunc <kristian.kunc@gmail.com>
2023-06-14 12:52:08 +0200

    move navbar + cleanup console logs

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,8 +3,7 @@ import type { LayoutServerLoad } from "./$types";
 export const load = (async ({ cookies }) => {
 	const sessionId = cookies.get("session");
 	const res = await fetch("https://api.vatnotif.kristn.co.uk");
-	console.log(sessionId);
-	console.log(cookies.getAll());
+
 	return {
 		isLoggedIn: sessionId ? true : false,
 		apiStatus: res.ok,
