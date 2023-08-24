39524fa9ac2b2ad99a7e66d6e52d0deba91a3d3d
Kristián Kunc <kristian.kunc@gmail.com>
2023-08-24 23:00:21 +0200

    remove callsign input validation

--- src/lib/scripts/input-validation.ts
+++ src/lib/scripts/input-validation.ts
@@ -1,6 +1,6 @@
 export class InputValidation {
 	public static isCallsign(callsign: string): boolean {
-		return /^[A-Z]{2,4}((\_|-)[A-Z0-9_]{0,2})?_?\_(CTR|APP|DEP|TWR|GND|DEL|TMU|FSS|RMP|FIS|FMP|AFIS)$/.test(callsign) && callsign.length <= 15;
+		return callsign.length <= 15;
 	}
 
 	public static isDiscordWebhookUrl(webhookUrl: string): boolean {
