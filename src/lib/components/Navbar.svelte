5570151c03a0b61e7ac79f499f87171f35d8518a
Kristián Kunc <kristian.kunc@gmail.com>
2024-07-02 14:06:14 +0200

    remove api status from navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,28 +1,27 @@
 <script lang="ts">
 	import { page } from "$app/stores";
 	import { signIn, signOut } from "@auth/sveltekit/client";
-	import { onMount } from "svelte";
-
-	let statusElemet: HTMLDivElement;
-	onMount(async () => {
-		const response = await fetch("https://api.vatnotif.kristn.co.uk/");
-		statusElemet.innerText = response.ok ? "API status: ✅" : "API status: ❌";
-	});
 </script>
 
-<nav class="align-center flex justify-between bg-primary-600 p-4 text-white">
+<nav class="flex justify-between bg-primary-600 p-4 text-white">
 	<div class="flex items-center text-2xl font-semibold">
 		<a href="/">
 			<h2>VatNotif</h2>
 		</a>
 	</div>
-	<div class="flex items-center" bind:this={statusElemet}>API status: ⏳</div>
 	<div class="flex items-center">
 		{#if $page.data.session?.user}
 			<p class="pr-2">{$page.data.session.user.name}</p>
-			<button class="rounded bg-secondary-600 px-4 py-2 text-white transition duration-150 ease-in-out hover:bg-secondary-700" on:click={signOut}
-				>Sign out</button
+			<button
+				class="rounded bg-secondary-600 px-4 py-2 text-white transition duration-150 ease-in-out hover:bg-secondary-700"
+				on:click={() => signOut()}>Sign out</button
 			>
+			<a href="/dashboard" class="pl-2">
+				<button
+					class="rounded bg-primary-400 px-4 py-2 text-white transition duration-150 ease-in-out hover:bg-primary-500
+				">Dashboard</button
+				>
+			</a>
 		{:else}
 			<button
 				class="rounded bg-accent-500 px-4 py-2 text-white transition duration-150 ease-in-out hover:bg-accent-700"
