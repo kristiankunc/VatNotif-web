4da852ee731e4b2b4bb897cb1ba7bf1af4813f5b
Kristián Kunc <kristian.kunc@gmail.com>
2024-07-09 16:54:20 +0200

    add plausible

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,6 +24,8 @@
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 
 	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
+
+	<script defer data-domain="vatnotif.kristn.co.uk" src="https://plausible.kristn.co.uk/js/script.js"></script>
 </svelte:head>
 
 <Navbar />
