f472db8f647eb97e1de3404db476493eb54189de
Kristián Kunc <kristian.kunc@gmail.com>
2024-07-09 13:44:32 +0200

    improve error message

--- src/lib/components/Error.svelte
+++ src/lib/components/Error.svelte
@@ -3,19 +3,31 @@
 
 	const title = writable("Error");
 	const message = writable("An error occurred");
-	let popver: HTMLDivElement;
+	const isVisible = writable(false);
+	let popover: HTMLDivElement;
 
 	export function showError(_title: string, _message: string) {
 		title.set(_title);
 		message.set(_message);
-		popver.showPopover();
+		popover.showPopover();
+		isVisible.set(true);
+	}
+
+	function hideError() {
+		popover.hidePopover();
+		isVisible.set(false);
 	}
 </script>
 
-<div bind:this={popver} popover="manual">
-	<div class="m-auto flex w-10/12 flex-col rounded-lg bg-red-500 p-4 text-white md:w-3/6">
-		<h2 class="w-full text-left text-3xl">{$title}</h2>
-		<p class="w-full text-left text-lg">{$message}</p>
-		<button on:click={() => popver.hidePopover()} class="m-2 rounded-lg bg-white p-2 text-red-500 hover:bg-red-100 hover:text-red-700">Roger</button>
+<div class="relative">
+	{#if $isVisible}
+		<div class="fixed inset-0 z-50 bg-black bg-opacity-35"></div>
+	{/if}
+	<div bind:this={popover} popover="manual" class="bg-transparent">
+		<div class="m-auto flex flex-col rounded-lg bg-red-500 p-4 text-white">
+			<h2 class="w-full text-left text-3xl">{$title}</h2>
+			<p class="w-full text-left text-lg">{$message}</p>
+			<button on:click={() => hideError()} class="m-2 rounded-lg bg-white p-2 text-red-500 hover:bg-red-100 hover:text-red-700">Roger</button>
+		</div>
 	</div>
 </div>
