99df40c6d0f9e4138efffab7beb86692dbb00523
Kristián Kunc <kristian.kunc@gmail.com>
2025-12-27 18:52:26 +0100

    allow for weirdass EHAA callsigns

--- src/lib/callsign.ts
+++ src/lib/callsign.ts
@@ -2,7 +2,7 @@ export function isCallsign(callsign: string): boolean {
 	if (callsign.length > 15) return false;
 
 	if (!callsign.includes("%"))
-		return /^[A-Z]{2,4}((\_|-)[A-Z0-9_]{0,2})?_?\_(CTR|APP|DEP|TWR|GND|DEL|TMU|FSS|RMP|FIS|FMP|AFIS|OBS|SUP)$/.test(callsign);
+		return /^[A-Z]{2,4}((\_|-)[A-Z0-9_]{0,3})?_?\_(CTR|APP|DEP|TWR|GND|DEL|TMU|FSS|RMP|FIS|FMP|AFIS|OBS|SUP)$/.test(callsign);
 
 	if (callsign.indexOf("%") < 2) return false;
 
